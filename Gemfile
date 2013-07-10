8e6d6c53aead0d3897902d73469e4b5e55ff91e0
Martin Natano <natano@natano.net>
2013-07-10 16:38:41 +0200

    require will_paginate gem

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'http://rubygems.org'
 
 gem 'her'
+gem 'will_paginate'
 
 group :development do
   gem 'rake'
--- lib/query-interface-client.rb
+++ lib/query-interface-client.rb
@@ -1,2 +1,4 @@
+require 'will_paginate/array'
+
 require 'query-interface-client/lazy_query'
 require 'query-interface-client/resource'
